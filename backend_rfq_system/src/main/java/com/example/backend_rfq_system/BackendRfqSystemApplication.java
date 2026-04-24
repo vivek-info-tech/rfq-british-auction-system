88b10f2ee8b2add0235d5e9354a213466e8e06a1
Vivek Kumar <vk01vivek@gmail.com>
2026-04-24 23:21:34 +0530

    frontend and backend project created

---
+++ backend_rfq_system/src/main/java/com/example/backend_rfq_system/BackendRfqSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_rfq_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRfqSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRfqSystemApplication.class, args);
+	}
+
+}
---
+++ backend_rfq_system/src/test/java/com/example/backend_rfq_system/BackendRfqSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_rfq_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRfqSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
