a5fac1c98eff6f35cd78181f15e311aed7360e77
Vivek Kumar <155941597+vivek-info-tech@users.noreply.github.com>
2026-06-25 22:19:32 +0530

    Change API base URL to production endpoint

--- frontend_rfq_system/src/utils/api.js
+++ frontend_rfq_system/src/utils/api.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
-const api = axios.create({ baseURL:'http://localhost:8080/api' });
+const api = axios.create({ baseURL:'https://auction-backend-system.onrender.com' });
 export default api;
